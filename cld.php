8e7be6d6facff7e7a58414049bf6a40d030043a7
Jeffry Degrande <jeffry@jeffrydegrande.com>
2016-06-07 20:42:42 -0700

    update php test script

--- cld.php
+++ cld.php
@@ -1,13 +1,28 @@
 <?php
 
-$functions = get_extension_funcs($module);
+$functions = get_extension_funcs('cld');
 print_r ($functions);
 
 $args = array (
-    "I Love you",
-    "Ik hou van je",
-    "Je t'aime",
-    "Eu te amo gatinha"
+    "Ik ben erg hongerig",
+    "I'm very hungry",
+    "جائع",
+    "Estou faminto",
+    "j'ai très faim",
+    "Tôi rất đói",
+    "මම ඉතා බඩගිනි",
+    "من خیلی گرسنه ام",
+    "나는 매우 배고파",
+    "म धेरै भोक",
+    "Im 'valde ieiunus",
+    "nagyon éhes vagyok",
+    "I Like loa pōloli",
+    "Mi tre malsatas",
+    "Iu sugnu 'na fami",
+    "Ako'y sobrang gutom",
+    "我好餓",
+    "我好饿",
+    "私はとてもお腹がすいてます"
 );
 
 foreach ($args as $arg) {
